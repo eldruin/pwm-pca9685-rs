b7eade2292a8d88a8a3286606b0ddfcbc2681709
Diego Barrios Romero <eldruin@gmail.com>
2019-11-21 18:26:27 +0100

    Update documentation

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ controller, based on the [`embedded-hal`] traits.
 This driver allows you to:
 - Enable/disable the device. See `enable()`.
 - Set the _on_ and _off_ counter for a channel or all of them. See `set_channel_on()`.
+- Set the _on_ and _off_ counters for a channel or all of them at once. See `set_channel_on_off()`.
 - Set a channel to be always on or off. See `set_channel_full_on()`.
+- Set the _on_ and _off_ counters for each channel at once. See `set_all_on_off()`.
 - Set the prescale value. See `set_prescale()`.
 - Select the output logic state direct or inverted. See `set_output_logic_state()`.
 - Select the EXTCLK pin as clock source. See `use_external_clock()`.
--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,18 @@
 //! This driver allows you to:
 //! - Enable/disable the device. See [`enable()`].
 //! - Set the _on_ and _off_ counter for a channel or all of them. See [`set_channel_on()`].
+//! - Set the _on_ and _off_ counters for a channel or all of them at once. See [`set_channel_on_off()`].
 //! - Set a channel to be always on or off. See [`set_channel_full_on()`].
+//! - Set the _on_ and _off_ counters for each channel at once. See [`set_all_on_off()`].
 //! - Set the prescale value. See [`set_prescale()`].
 //! - Select the output logic state direct or inverted. See [`set_output_logic_state()`].
 //! - Select the EXTCLK pin as clock source. See [`use_external_clock()`].
 //!
 //! [`enable()`]: struct.Pca9685.html#method.enable
 //! [`set_channel_on()`]: struct.Pca9685.html#method.set_channel_on
+//! [`set_channel_on_off()`]: struct.Pca9685.html#method.set_channel_on_off
 //! [`set_channel_full_on()`]: struct.Pca9685.html#method.set_channel_full_on
+//! [`set_all_on_off()`]: struct.Pca9685.html#method.set_all_on_off
 //! [`set_prescale()`]: struct.Pca9685.html#method.set_prescale
 //! [`set_output_logic_state()`]: struct.Pca9685.html#method.set_output_logic_state
 //! [`use_external_clock()`]: struct.Pca9685.html#method.use_external_clock
@@ -104,6 +108,35 @@
 //! let mut pwm = Pca9685::new(dev, address);
 //! pwm.set_prescale(100).unwrap();
 //!
+//! // Turn on channel 0 at 0 and off at 2047, which is 50% in the range `[0..4095]`.
+//! pwm.set_channel_on_off(Channel::C0, 0, 2047).unwrap();
+//!
+//! // Turn on channel 1 at 200, then off at 3271. These values comes from:
+//! // 0.000814 (seconds) * 60 (Hz) * 4096 (resolution) = 200
+//! // 4096 * 0.75 + 200 = 3272
+//! pwm.set_channel_on_off(Channel::C1, 200, 3272).unwrap();
+//! # }
+//! ```
+//!
+//! ### Set the PWM frequency and channel duty cycles separately
+//!
+//! - Set a PWM frequency of 60 Hz (corresponds to a value of 100 for the
+//!   prescale).
+//! - Set a duty cycle of 50% for channel 0.
+//! - Set a duty cycle of 75% for channel 1 delayed 814 µs with respect
+//!   to channel 0.
+//!
+//! ```no_run
+//! extern crate linux_embedded_hal as hal;
+//! extern crate pwm_pca9685 as pca9685;
+//! use pca9685::{ Channel, Pca9685, SlaveAddr };
+//!
+//! # fn main() {
+//! let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
+//! let address = SlaveAddr::default();
+//! let mut pwm = Pca9685::new(dev, address);
+//! pwm.set_prescale(100).unwrap();
+//!
 //! // Turn on channel 0 at 0
 //! pwm.set_channel_on(Channel::C0, 0).unwrap();
 //!
@@ -150,6 +183,25 @@
 //! # }
 //! ```
 //!
+//! ### Set the channel duty cycle for all channels at once
+//!
+//! - Set a duty cycle of 50% on all channels
+//!
+//! ```no_run
+//! extern crate linux_embedded_hal as hal;
+//! extern crate pwm_pca9685 as pca9685;
+//! use pca9685::{ Channel, Pca9685, SlaveAddr };
+//!
+//! # fn main() {
+//! let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
+//! let address = SlaveAddr::default();
+//! let mut pwm = Pca9685::new(dev, address);
+//!
+//! let mut on = [0; 16];
+//! let mut off = [2047; 16];
+//! pwm.set_all_on_off(&on, &off);
+//! # }
+//! ```
 
 #![deny(missing_docs, unsafe_code)]
 #![no_std]
